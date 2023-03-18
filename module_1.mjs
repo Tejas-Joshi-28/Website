e34357da0a4343f42f4e96aecaae5847e82d901c
TejasJoshi-web <112250892+TejasJoshi-web@users.noreply.github.com>
2023-03-18 19:12:07 +0530

    Add files via upload

---
+++ module_1.mjs
@@ -0,0 +1,9 @@
+let a = "Harry"
+let b = "Rohan"
+let c = "Aakash"
+let d = "Priyanka"
+
+export default c
+export  {a}
+export  {b}
+export  {d}
---
+++ module_2.mjs
@@ -0,0 +1,12 @@
+// ******* we can use any name to use default export variable **************
+// Note :- Only one default export allowed per module.
+
+import xyz, {a,b,d} from './module_1.mjs'
+console.log(xyz)        // <--- Aakash
+
+
+console.log(`Value of a is :- ` + a)        // Value of a is :- Harry 
+
+console.log(`Value of b is :- ` + b)        // Value of b is :- Rohan   
+
+console.log(`Value of d is :- ` + d)        // Value of d is :- Priyanka
